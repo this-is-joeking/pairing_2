540156a62986f7ae1df9ef0720c85664c97c1bb3
Joe King <j.king92@gmail.com>
2022-09-14 14:16:30 -0700

    Add cities variable

--- friends.rb
+++ friends.rb
@@ -1,6 +1,13 @@
 arrays = ["Jordan", "Roman", "Adam", "Ryan", "Matt"]
 
 puts arrays[2]
-
 puts arrays[4]
 puts arrays[0]
+
+cities = ["Toronto", "NYC", "LA", "Paris", "Berlin"]
+
+puts cities
+
+cities.push ["Denver"]
+
+puts cities
